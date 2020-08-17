d976b6c13f1e17fa356191e5a0e96cbf466e7695
Xanadu Zhang <3369760+yeahnoob@users.noreply.github.com>
2020-08-17 19:16:14 +0800

    more complete Arrow Function, compared with Normal Function

--- arrow-function/arrow-with-this.js
+++ arrow-function/arrow-with-this.js
@@ -1,23 +1,45 @@
+/*
+ * ## Arrow function expressions
+ * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
+ *
+ * An arrow function expression is a syntactically compact alternative to a regular function expression
+ * , although without its own bindings to the this, arguments, super, or new.target keywords.
+ * Arrow function expressions are ill suited as methods, and they cannot be used as constructors.
+ *
+ */
+
 class Person {
   constructor(name) {
     this.name = name;
   }
 
-  getNameArrow = () => {
-    setTimeout(() => {
-      console.log('.. Arrow Function .. ' + this.name);
+  // Use non-arrow functions for methods that will be called using the object.method() syntax. Those are the functions that will receive a meaningful this value from their caller.
+  addSuffix(suffix) {
+    setTimeout(function() {
+      console.log('.. Normal Function with `this.` .. \n' + suffix + this.name);
     }, 100);
-    return this.name;
   }
 
-  getName = function() {
+  // In JS Bad history, hack `self.` for `this.` in normal funcion.
+  addSuffixHistory(suffix) {
+    const self = this;
     setTimeout(function() {
-      console.log('.. Normal Function .. ' + this.name);
+      console.log('.. Normal Function with hacking `self.` for `this.` .. \n' + suffix + self.name);
+    }, 100);
+
+  }
+
+  // Use arrow functions for everything else.
+  addSuffixArrow = (suffix) => {
+    setTimeout(() => {
+      console.log('.. Arrow Function with `this.`.. \n' + suffix + this.name);
     }, 100);
   }
+
 }
 
-let person = new Person('Bob');
-person.getNameArrow();
-person.getName();
-console.log('.. Without Function .. ' + this.name);
+const name = 'Bob';
+let person = new Person(name);
+person.addSuffix('Dr. ');
+person.addSuffixHistory('Dr. ');
+person.addSuffixArrow('General. ');
